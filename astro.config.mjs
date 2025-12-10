8af9ccaf04cbd6b780b47a3f2c56ec754f527718
Devkuroi <joan2000oviedo@gmail.com>
2025-12-10 17:45:52 -0500

    segundo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://devkuroi.github.io",
+  base: "proyecto-sistemas-operativos"
 });
